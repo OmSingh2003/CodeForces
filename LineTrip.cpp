ea34d113574804624cb6fe4e6f5f4745751f6003
Om Singh <omsingh.ailearn@gmail.com>
2025-07-22 14:00:39 +0530

    cp

---
+++ LineTrip.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, x;
+        cin >> n >> x;
+        vector<long long> points;
+        points.push_back(0); 
+        for (int i = 0; i < n; ++i) {
+            long long a;
+            cin >> a;
+            points.push_back(a);
+        }
+        points.push_back(x);  
+        sort(points.begin(), points.end());  
+
+        long long required_tank = 0;
+        for (int i = 1; i < points.size(); ++i) {
+            required_tank = max(required_tank, points[i] - points[i - 1]);
+        }
+
+        cout << required_tank << endl;
+    }
+    return 0;
+}
