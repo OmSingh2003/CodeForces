8e6c110764ac39d51c703ecb7fb4acd0e028fe68
Om Singh <omsingh.ailearn@gmail.com>
2025-07-26 09:12:45 +0530

    cp

--- Practice/graph.cpp
+++ Practice/graph.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include<unordered_map>
 #include<string>
+#include<unordered_set>
+#include<queue>
 using namespace std;
 
 struct graph {
@@ -19,7 +21,37 @@ void printGraph(const graph& g){
         cout<<endl;
     }
 }
+void dfs(const graph& g,const string& node, unordered_set<string>& visited){ // took a graph(reference variable) starting node which is also referenced and then visited set to avoid cycle
+    visited.insert(node);// insert out current node in visited 
+    cout<<node<<" "; // print node
+    for(const string& neighbour : g.adjList.at(node)){// traverse 
+        if (visited.find(neighbour)==visited.end()){
+            dfs(g,neighbour,visited);
+        }
+    }
+}
 
+void bfs(const graph& g, const string& startNode) {
+    unordered_set<string> visited;
+    queue<string> q;
+    visited.insert(startNode);
+    q.push(startNode);
+    while(!q.empty()){
+        string node = q.front();
+        q.pop();
+        cout << node <<" ";
+
+        if (g.adjList.find(node) != g.adjList.end()) {
+            for (const string& neighbour : g.adjList.at(node)) {
+                if (visited.find(neighbour)==visited.end()){
+                    visited.insert(neighbour);
+                    q.push(neighbour);
+                }
+            }
+        }
+    }
+
+}
 int main(){
     graph g;
     addEdge(g,"A","B");
